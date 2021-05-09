8f60d4ee149692d9830234f7b146e23acb4e18b7
NatalijaGajic <nat.gaj98@gmail.com>
2021-05-09 23:01:21 +0200

    Folder names fixed

---
+++ ReservationSystem/validators/TableUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ReservationSystem.Core.dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.validators
+{
+    public class TableUpdateDto: AbstractValidator<GameCreationDto>
+    {
+    }
+}
